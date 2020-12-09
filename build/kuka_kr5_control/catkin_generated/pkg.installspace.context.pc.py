663512166aba5178c29c3c686b9119fc8eda3708
Preetham Sura <preethamss@berkeley.edu>
2020-12-09 15:40:30 -0800

    Removed build and devel

--- build/kuka_kr5_control/catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "${prefix}/include".split(';') if "${prefix}/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "std_msgs;roscpp;tf;std_msgs;geometry_msgs;nav_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lkuka_kr5_control".split(';') if "-lkuka_kr5_control" != "" else []
-PROJECT_NAME = "kuka_kr5_control"
-PROJECT_SPACE_DIR = "/home/rafi/ros_workspaces/final_project/c106a-project2/install"
-PROJECT_VERSION = "0.0.0"
